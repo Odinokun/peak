b691117aed0e41842f03b378d75979bda4992d83
odinokun <odinokun@gmail.com>
2024-09-24 21:27:18 -0400

    enhance WhatsApp visibility toggle with pointer events for better interaction

--- src/js/modules/hide2scroll.js
+++ src/js/modules/hide2scroll.js
@@ -3,8 +3,10 @@ module.exports = function () {
     var element = document.getElementById('top__whatsapp-text');
     if (window.scrollY > 250) {
       element.style.opacity = '0'; // Скрыть элемент
+      element.style.pointerEvents = 'none'; // Скрыть элемент
     } else {
       element.style.opacity = '1'; // Показать элемент
+      element.style.pointerEvents = 'auto'; // Показать элемент
     }
   });
 };
